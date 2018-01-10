8e2522fc9dcc49b45f8102d2dfc082ba24db178d
JamesJeffryes <jamesgjeffryes@gmail.com>
2018-01-10 08:21:47 -0600

    use GFU to save genome

--- lib/Bio/KBase/kbaseenv.pm
+++ lib/Bio/KBase/kbaseenv.pm
@@ -5,6 +5,7 @@ use Bio::KBase::utilities;
 
 our $ws_client = undef;
 our $ga_client = undef;
+our $gfu_client = undef;
 our $ac_client = undef;
 our $data_file_client = undef;
 our $objects_created = [];
@@ -112,6 +113,18 @@ sub ga_client {
 		$ga_client = new GenomeAnnotationAPI::GenomeAnnotationAPIClient(Bio::KBase::utilities::utilconf("call_back_url"));
 	}
 	return $ga_client;
+}
+
+sub gfu_client {
+	my($parameters) = @_;
+	$parameters = Bio::KBase::utilities::args($parameters,[],{
+		refresh => 0
+	});
+	if ($parameters->{refresh} == 1 || !defined($gfu_client)) {
+		require "GenomeFileUtil/GenomeFileUtilClient.pm";
+		$gfu_client = new GenomeFileUtil::GenomeFileUtilClient(Bio::KBase::utilities::utilconf("call_back_url"));
+	}
+	return $gfu_client;
 }
 
 sub ac_client {
