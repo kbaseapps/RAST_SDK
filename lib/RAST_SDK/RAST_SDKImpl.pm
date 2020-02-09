5324be5e7779e4db6686c2353d77bb7bdc7db826
qzzhang <qz_zhang@yahoo.com>
2020-02-08 21:56:15 -0800

    added class variables and constructor/init functions to metag_utils

--- lib/RAST_SDK/RAST_SDKImpl.pm
+++ lib/RAST_SDK/RAST_SDKImpl.pm
@@ -33,6 +33,7 @@ use Bio::KBase::GenomeAnnotation::Service;
 use LWP::UserAgent;
 use HTTP::Request;
 
+use lib '../lib';
 use metag_utils;
 
 
@@ -1838,7 +1839,7 @@ sub annotate_metagenome
     my $config_file = $ENV{ KB_DEPLOYMENT_CONFIG };
     my $config = new Config::Simple($config_file)->get_block('RAST_SDK');
 
-    my $mg_util = metag_utils::new('config'=>$config, 'ctx'=>$ctx);
+    my $mg_util = new metag_utils($config, $ctx);
     my $metag_ref = $mg_util->rast_metagenome($params);
     $output = {
         "output_metagenome_ref" => $metag_ref,
