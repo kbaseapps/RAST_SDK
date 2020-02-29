dd3c543c0667c83ef41ede8aa64133ec543945db
Qizhi Zhang <qzzhang@users.noreply.github.com>
2020-02-28 19:08:47 -0600

    Update RAST_SDKImpl.pm

--- lib/RAST_SDK/RAST_SDKImpl.pm
+++ lib/RAST_SDK/RAST_SDKImpl.pm
@@ -1470,13 +1470,6 @@ sub annotate_genomes
 	    call_features_prophage_phispy => 1,
 	    retain_old_anno_for_hypotheticals => 1
 	});
-	
-	my $empty_input_msg = ("Required inputs: genomes in an array with at least one genome;\n".
-	                       "and/or a list of genome names separated by ';', '\n' or '|' (without quotes).\n";
-	Bio::KBase::Exceptions::ArgumentValidationError->throw(
-            error        => $empty_input_msg,
-            method_name  => 'annotate_genomes'
-        ) unless (is_arrayref( $genomes ) && @$genomes) && defined($params->{genome_text});
 
 	if ($params->{ncbi_taxon_id}) {
 		$params->{scientific_name} = $self->get_scientific_name_for_NCBI_taxon(
@@ -1487,6 +1480,14 @@ sub annotate_genomes
 	my $genomes = $params->{input_genomes};
 
 	my $obj_type;
+		
+	my $empty_input_msg = ("Required inputs: genomes in an array with at least one genome;\n".
+	                       "and/or a string of genome names separated by ';', '\n' or '|' (without quotes).\n";
+	Bio::KBase::Exceptions::ArgumentValidationError->throw(
+            error        => $empty_input_msg,
+            method_name  => 'annotate_genomes'
+        ) unless is_arrayref( $genomes ) && @$genomes && $params->{ genome_text };
+
 	#
 	# If $genomes is an ARRAY, then multiple genomes or assemblies or sets were submitted
 	#
@@ -1498,7 +1499,7 @@ sub annotate_genomes
 	#	4. Use perl grep to see if the ref is already in the list
 	#	5. Issue a warning when a duplicate is found so user knows what happened. 
 	#
-	if ( ref $genomes eq 'ARRAY' && @$genomes ) {
+	if ( is_arrayref( $genomes ) && @$genomes ) {
 		my $replace_genomes = [];
 		foreach my $ref (@$genomes) {
 	 		my $info = Bio::KBase::kbaseenv::get_object_info([{ref=>$ref}],0);
