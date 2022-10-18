5180671fefe5a582b503969b246736173b0053ce
Christopher Henry <chenry@mcs.anl.gov>
2022-10-18 13:42:43 -0500

    Fixing numbering and repetition of rna features

--- lib/RAST_SDK/AnnotationUtils.pm
+++ lib/RAST_SDK/AnnotationUtils.pm
@@ -2171,6 +2171,9 @@ sub _compute_genome_assembly_stats {
 							$ftr->{location}->[0]->[3]
 						} = 1;
 						$new_array.append($ftr);
+						my $array = [split(/\./,$ftr->{id})];
+						@{$array}.pop();
+						$ftr->{id} = join(".",@{$array}).".".len(@{$new_array});
 						if ($ftr->{location}->[0]->[2] eq "+") {
 							$ftr->{dna_sequence} = uc substr $contighash->{$ftr->{location}->[0]->[0]}, $ftr->{location}->[0]->[1], $ftr->{location}->[0]->[3];
 						} else {
